20700fa31f60149eefe4883bfeb157bb6cdfec98
rodrigo carvalho <rodrigo.silva1516@etec.sp.gov.br>
2022-10-27 21:53:52 -0300

    projeto inicial

---
+++ rcs27-10-22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rcs27_10_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int numero2;
+            int total;
+
+            numero = 10;
+            numero2 = 40;
+
+            total = numero + numero2;
+
+            Console.WriteLine("Boa noite 1ºB informática");
+            Console.WriteLine("vamo que vamo galera");
+            Console.WriteLine("24 / 10 / 2022 Segunda Feira");
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+    }
+}
